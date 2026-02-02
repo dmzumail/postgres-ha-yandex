138f7ab2f2311f2cdbd98f1b355a30272bf839be
dmzumail <dmzumail@gmail.com>
2026-02-02 13:34:57 +0300

    feat(infra): добавить вывод внутренних IP и обновить инвентарь для etcd

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -5,22 +5,29 @@ ROOT_DIR="$(dirname "$(realpath "$0")")/.."
 mkdir -p "$ROOT_DIR/ansible/inventory"
 cd "$ROOT_DIR/terraform"
 
-# Получаем IP из instance_ips с помощью jq
-PG1=$(terraform output -json instance_ips | jq -r '.["pg-node-1"]')
-PG2=$(terraform output -json instance_ips | jq -r '.["pg-node-2"]')
-PG3=$(terraform output -json instance_ips | jq -r '.["pg-node-3"]')
+# Получаем публичные и внутренние IP
+PG1_PUBLIC=$(terraform output -json instance_ips | jq -r '.["pg-node-1"]')
+PG2_PUBLIC=$(terraform output -json instance_ips | jq -r '.["pg-node-2"]')
+PG3_PUBLIC=$(terraform output -json instance_ips | jq -r '.["pg-node-3"]')
+
+PG1_INTERNAL=$(terraform output -json instance_internal_ips | jq -r '.["pg-node-1"]')
+PG2_INTERNAL=$(terraform output -json instance_internal_ips | jq -r '.["pg-node-2"]')
+PG3_INTERNAL=$(terraform output -json instance_internal_ips | jq -r '.["pg-node-3"]')
 
 cat > "$ROOT_DIR/ansible/inventory/hosts.yml" << EOF
 postgresql:
   hosts:
     pg-node-1:
-      ansible_host: $PG1
+      ansible_host: $PG1_PUBLIC
+      internal_ip: $PG1_INTERNAL
       etcd_name: node1
     pg-node-2:
-      ansible_host: $PG2
+      ansible_host: $PG2_PUBLIC
+      internal_ip: $PG2_INTERNAL
       etcd_name: node2
     pg-node-3:
-      ansible_host: $PG3
+      ansible_host: $PG3_PUBLIC
+      internal_ip: $PG3_INTERNAL
       etcd_name: node3
   vars:
     ansible_user: ubuntu
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,6 +5,13 @@ output "instance_ips" {
   }
 }
 
+output "instance_internal_ips" {
+  value = {
+    for i, instance in yandex_compute_instance.pg :
+    "pg-node-${i + 1}" => instance.network_interface[0].ip_address
+  }
+}
+
 output "backup_bucket" {
   value = yandex_storage_bucket.pg_backups.bucket
 }
